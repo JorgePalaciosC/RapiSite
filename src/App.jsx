d100fe4111bc430026399bdb73764489c447a052
Jorge Palacios <jorge_palacios_c@outlook.com>
2025-11-25 12:52:27 -0600

    cambios de tamaño en fuentes

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,17 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Layout from "./components/Layout";
 import Home from "./components/pages/Home";
 import About from "./components/pages/About";
 
 function App() {
-
-
   return (
-    <>
-     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route path="about" element={<About />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
-    </>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Home />} />
+        <Route path="about" element={<About />} />
+      </Route>
+    </Routes>
   )
-};
+}
 
-export default App
+export default App;
